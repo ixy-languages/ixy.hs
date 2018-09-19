1ebf032bb29fffbc402b07dcd802ace3a25c6645
Alex Egger <alex.egger96@gmail.com>
2018-09-19 19:41:25 +0200

    Add mkBusDeviceFunction and fix references to Text

--- src/Lib/Pci.hs
+++ src/Lib/Pci.hs
@@ -7,17 +7,22 @@ import Lib.Prelude hiding (writeFile)
 import Control.Monad.Catch (MonadCatch, handleIf)
 import Data.Bits ((.|.), shift)
 import qualified Data.ByteString as B
-import Data.Text as Text
+import qualified Data.Text as Text
 import System.IO.Error (isDoesNotExistError)
 import System.IO.MMap (Mode(ReadWrite), mmapFilePtr)
 import qualified System.Path as Path
 import System.Path ((</>))
-import qualified System.Path.Directory as Dir
 import qualified System.Path.IO as PathIO
+import Text.Regex.PCRE ((=~))
 
 newtype BusDeviceFunction = BDF
     { unBusDeviceFunction :: Text
     }
+
+mkBusDeviceFunction :: Text -> Maybe BusDeviceFunction
+mkBusDeviceFunction bdfText
+    | Text.unpack bdfText =~ Text.unpack "[0-9A-F]{4}:[0-9A-F]{2}:[0-9A-F]{2}.[0-9]" = Just (BDF bdfText)
+mkBusDeviceFunction _ = Nothing
 
 base :: Path.AbsDir
 base = Path.dirPath "/sys/bus/pci/devices/"
@@ -33,7 +38,7 @@ enableDMA bdf =
              value <- B.hGet h 2
              B.hPut h $ setDMA value)
   where
-    path = base </> Path.relPath (unpack bdfT) </> Path.filePath "config"
+    path = base </> Path.relPath (Text.unpack bdfT) </> Path.filePath "config"
     bdfT = unBusDeviceFunction bdf
     cmdRegOffset = 4
     busMasterEnableIndex = 2
@@ -47,12 +52,12 @@ mapResource bdf resource =
         (ptr, _, _, _) <- mmapFilePtr (Path.toString path) ReadWrite Nothing
         return ptr
   where
-    path = base </> Path.relPath (unpack bdfT) </> resource
+    path = base </> Path.relPath (Text.unpack bdfT) </> resource
     bdfT = unBusDeviceFunction bdf
 
 unbind :: (MonadCatch m, MonadIO m) => BusDeviceFunction -> m ()
-unbind bdf = handleIf isDoesNotExistError handler (liftIO $ PathIO.writeFile path $ unpack bdfT)
+unbind bdf = handleIf isDoesNotExistError handler (liftIO $ PathIO.writeFile path $ Text.unpack bdfT)
   where
-    path = base </> Path.relPath (unpack bdfT) </> Path.filePath "driver/unbind"
+    path = base </> Path.relPath (Text.unpack bdfT) </> Path.filePath "driver/unbind"
     bdfT = unBusDeviceFunction bdf
     handler _ = return ()
