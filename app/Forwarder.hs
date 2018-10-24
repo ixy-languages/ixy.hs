bbf3fc1f33995de66a5f71db668da07315d2414d
Alex Egger <alex.egger96@gmail.com>
2018-10-24 21:23:51 +0200

    Pimp my forwarder (S02E01)

--- app/Forwarder.hs
+++ app/Forwarder.hs
@@ -11,6 +11,7 @@ import           Data.IORef
 import           Data.Maybe
 import qualified Data.Vector                   as V
 import           Protolude
+import           System.Clock
 
 newtype App a = App { runApp :: LoggingT IO a } deriving (Functor, Applicative, Monad, MonadIO, MonadCatch, MonadThrow, MonadLogger)
 
@@ -22,26 +23,36 @@ run = do
   dev1    <- fromJust <$> newDriver "0000:02:00.0" 1 1
   dev2    <- fromJust <$> newDriver "0000:02:00.1" 1 1
   counter <- liftIO $ newIORef (0 :: Int)
-  loop counter dev1 dev2
+  liftIO $ loop counter dev1 dev2
 
-loop :: IORef Int -> Device -> Device -> App ()
-loop counter dev1 dev2 = forever $ do
-  liftIO $ forward dev1 dev2
-  liftIO $ forward dev2 dev1
-  liftIO $ modifyIORef' counter (+ 1)
-  c <- liftIO $ readIORef counter
-  when
-    (c .&. 0xFFF == 0)
-    (liftIO $ do
-      st1 <- stats dev1
-      st2 <- stats dev2
-      putStrLn (show st1 :: Text)
-      putStrLn (show st2 :: Text)
-    )
+loop :: IORef Int -> Device -> Device -> IO ()
+loop counter dev1 dev2 = do
+  let clock = Monotonic
+  timeRef <- newIORef (TimeSpec {sec = 0, nsec = 0})
+  forever $ do
+    forward dev1 dev2
+    forward dev2 dev1
+    c <- readIORef counter
+    when
+      (c .&. 0xFF == 0)
+      (do
+        t          <- getTime clock
+        beforeTime <- readIORef timeRef
+        when
+          (sec (diffTimeSpec t beforeTime) >= 1)
+          (do
+            st1 <- stats dev1
+            st2 <- stats dev2
+            putStrLn (show st1 :: Text)
+            putStrLn (show st2 :: Text)
+          )
+        writeIORef timeRef t
+      )
+    modifyIORef' counter (+ 1)
 
 forward :: Device -> Device -> IO ()
 forward rxDev txDev = do
-  pkts <- receive rxDev (QueueId 0) 64
+  pkts <- receive rxDev (QueueId 0) 32
   unless
     (V.null pkts)
     (do
