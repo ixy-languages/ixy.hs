0a7d0c1ff0bc2cd50fb0e0d69a197cb874174ff3
Alex Egger <alex.egger96@gmail.com>
2019-01-27 15:48:06 +0100

    Add asm-less-opt to plots

--- measurements/plot-batch-16.R
+++ measurements/plot-batch-16.R
@@ -4,9 +4,12 @@ library(reshape2)
 
 dataAsm <- read.csv("./asm/raw-batch-16.csv", header=FALSE)
 dataLlvm <- read.csv("./llvm/raw-batch-16.csv", header=FALSE)
+dataAsmLess <- read.csv("./asm-less-opt/raw-batch-16.csv", header=FALSE)
 data <- merge(dataAsm, dataLlvm, by="V1")
+data <- merge(data, dataAsmLess, by="V1")
 data$V2.x <- data$V2.x * 2
 data$V2.y <- data$V2.y * 2
+data$V2 <- data$V2 * 2
 
 b <- c(1,2,4,8,16,32,64,128,256)
 
@@ -15,6 +18,6 @@ d <- melt(data, id.vars=c("V1"))
 png("hs-batch-16.png")
 p <- ggplot(d) + geom_line(aes(x=V1, y=value, color=variable), size=2) + scale_x_continuous(trans="log2", breaks = b, labels = b) +
     xlab("Batch size") + ylab("Packet rate [Mpps]") + ggtitle("Packet rate for different batch sizes (@ 1.6GHz)") +
-    scale_y_continuous(limits=c(0, 12)) + scale_colour_manual(name="Backend", labels=c("GHC Native", "LLVM"), values=c("#396ab1", "#922428")) + 
+    scale_y_continuous(limits=c(0, 12)) + scale_colour_manual(name="Backend", labels=c("GHC Native (-O2)", "LLVM", "GHC Native (-O1)"), values=c("#396ab1", "#3e9651", "#922428")) + 
     geom_point(aes(x=V1, y=value, color=variable), size=3)
 print(p)
